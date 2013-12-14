eb3617a7f2d3b3e4a84707115abe5ba7384aed04
Sören Gade <gade.soeren@googlemail.com>
2013-12-14 14:34:07 +0100

    Added game class.

---
+++ src/Game.lua
@@ -0,0 +1,12 @@
+local class = require 'lib/middleclass'
+local Drawable = require 'Drawable'
+
+Game = class('Game', Drawable)
+
+function Game:initialize()
+  Drawable:initialize(world)
+end
+
+function Game:render()
+  love.graphics.print('Hello World', 10, 10)
+end
