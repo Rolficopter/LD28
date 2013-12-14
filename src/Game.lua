e4867baac881f8d96d92db78db1e2e2f75c1d9cb
Sören Gade <gade.soeren@googlemail.com>
2013-12-14 14:40:02 +0100

    Added world (physics).

--- src/Game.lua
+++ src/Game.lua
@@ -3,10 +3,17 @@ local Drawable = require 'Drawable'
 
 Game = class('Game', Drawable)
 
+local initWorld = function(self)
+  self.world = love.physics.newWorld(0, 9.81, true)
+end
 function Game:initialize()
-  Drawable:initialize(world)
+  initWorld(self)
+  Drawable:initialize(self.world)
 end
 
+function Game:update(dt)
+  self.world:update(dt)
+end
 function Game:render()
   love.graphics.print('Hello World', 10, 10)
 end
