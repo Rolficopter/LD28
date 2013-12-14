e3247cf7722010510a57593451bee03f03a55da1
Sören Gade <gade.soeren@googlemail.com>
2013-12-14 14:55:02 +0100

    Added color to Drawable.

--- src/Drawable.lua
+++ src/Drawable.lua
@@ -3,7 +3,8 @@ local class = require 'lib/middleclass'
 Drawable = class('Drawable')
 
 function Drawable:initialize(world)
-  
+  self.color = { 255, 255, 255, 255 }
+  self._drawColor = nil
 end
 
 function Drawable:update(dt)
@@ -12,6 +13,16 @@ end
 
 function Drawable:render()
   -- stub
+end
+
+-- Render Helpers Functions
+function Drawable:applyColor()
+  local r, g, b, a = love.graphics.getColor()
+  self._drawColor = { r, g, b, a }
+  love.graphics.setColor(self.color)
+end
+function Drawable:resetColor()
+  love.graphics.setColor(self._drawColor)
 end
 
 return Drawable
