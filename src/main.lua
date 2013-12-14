1ffeafcce2cc8003a1a93ce008f4869436b8b047
Sören Gade <gade.soeren@googlemail.com>
2013-12-14 14:33:52 +0100

    Added game instance.

--- src/main.lua
+++ src/main.lua
@@ -3,14 +3,18 @@
   main.lua
 ]]--
 
+local Game = require 'Game'
+
+local game = nil
+
 function love.load()
-  
+  game = Game:new()
 end
 
 function love.draw()
-  
+  game:render()
 end
 
-function love.update()
-  
+function love.update(dt)
+  game:update(dt)
 end
