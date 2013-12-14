48da50bb6b44aeacee6f332f0a1c2b585bdf188f
Jan-Henrik <jh.bruhn97@gmail.com>
2013-12-14 20:27:37 +0100

    removed unnecessary tempworld code

--- src/World.lua
+++ src/World.lua
@@ -8,18 +8,6 @@ local atl = require 'lib/advanced-tiled-loader/loader'
 
 World = class('World', Drawable)
 
--- Init logic
-local createTempWorld = function(self)
-  self.ground = {}
-  self.ground = Entity:new(self.world)
-  self.ground.color = { 50, 230, 50, 230 }
-  self.ground.body = love.physics.newBody(self.world, Constants.SCREEN.WIDTH / 2, Constants.SCREEN.HEIGHT - 50, 'static')
-  self.ground.shape = love.physics.newRectangleShape(Constants.SCREEN.WIDTH * 2, 100)
-  self.ground.fixture = love.physics.newFixture(self.ground.body, self.ground.shape)
-  
-  self.player = Player:new(self.world, 10 + Constants.SIZES.PLAYER.X / 2, 10 + Constants.SIZES.PLAYER.Y / 2)
-end
-
 function World:initialize()
   love.physics.setMeter(Constants.SIZES.METER)
   self.world = love.physics.newWorld(Constants.GRAVITY.X * Constants.SIZES.METER, Constants.GRAVITY.Y * Constants.SIZES.METER, true)
