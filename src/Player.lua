4a88d6f7b12b0659285480c08765299b1c07a1c3
Jan-Henrik <jh.bruhn97@gmail.com>
2013-12-14 20:05:15 +0100

    added test player texture

--- src/Player.lua
+++ src/Player.lua
@@ -12,7 +12,11 @@ function Player:initialize(world, x, y)
   self.shape = love.physics.newRectangleShape(Constants.SIZES.PLAYER.X, Constants.SIZES.PLAYER.Y)
   self:createFixture()
 
+  self.body:setFixedRotation(true)
+
   self.color = {255, 0, 0, 255}
+
+  self.texture = love.graphics.newImage('assets/textures/man.png')
 end
 
 function Player:update(dt)
@@ -21,6 +25,10 @@ function Player:update(dt)
   elseif love.keyboard.isDown("a") then
     self.body:applyForce(-400, 0)
   end
+end
+
+function Player:render()
+  love.graphics.draw(self.texture, self.body:getX() - Constants.SIZES.PLAYER.X/2, self.body:getY() -  Constants.SIZES.PLAYER.Y/2, 0, Constants.SIZES.PLAYER.X / self.texture:getWidth(), Constants.SIZES.PLAYER.Y / self.texture:getHeight())
 end
 
 return Player
