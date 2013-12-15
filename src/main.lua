a77a182123b551e29092bdc590c87ab73f596820
Jan-Henrik <jh.bruhn97@gmail.com>
2013-12-15 09:52:39 +0100

    added some keyboard helper functions

--- src/main.lua
+++ src/main.lua
@@ -17,4 +17,38 @@ end
 
 function love.update(dt)
   game:update(dt)
+
+  love.keyboard.updateKeys()
+end
+
+love.keyboard.keysPressed = { }
+love.keyboard.keysReleased = { }
+
+function love.keyboard.wasPressed(key)
+  if (love.keyboard.keysPressed[key]) then
+    return true
+  else
+    return false
+  end
+end
+
+function love.keyboard.wasReleased(key)
+  if (love.keyboard.keysReleased[key]) then
+    return true
+  else
+    return false
+  end
+end
+
+function love.keypressed(key, unicode)
+  love.keyboard.keysPressed[key] = true
+end
+
+function love.keyreleased(key)
+  love.keyboard.keysReleased[key] = true
+end
+
+function love.keyboard.updateKeys()
+  love.keyboard.keysPressed = { }
+  love.keyboard.keysReleased = { }
 end
