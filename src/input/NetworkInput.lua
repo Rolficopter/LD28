1948ea0d60d119b8028d3d151f9725d3c6867b3f
Sören Gade <gade.soeren@googlemail.com>
2013-12-15 14:48:03 +0100

    Added NetworkInput class.

---
+++ src/input/NetworkInput.lua
@@ -0,0 +1,20 @@
+local class = require 'lib/middleclass'
+local InputSource = require 'input/InputSource'
+
+NetworkInput = class('NetworkInput', InputSource)
+
+function NetworkInput:initialize(world)
+	InputSource:initialize(world)
+end
+
+function NetworkInput:shouldJump()
+	return false
+end
+
+function NetworkInput:getDirection()
+	return InputSource.Direction.none
+end
+
+function NetworkInput:getArmAngle()
+	return 0
+end
