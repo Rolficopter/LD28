f05f231f472b3d954cab80e63f5f64251638d33c
Jan-Henrik <jh.bruhn97@gmail.com>
2013-12-15 10:03:45 +0100

    implemented smooth cursor in main menu

--- src/menu/MainMenu.lua
+++ src/menu/MainMenu.lua
@@ -14,6 +14,16 @@ function MainMenu:initialize()
 
   self.textStartGame = "Start Game"
   self.textQuit = "Quit"
+
+  self.cursor = {}
+  self.cursor.x = 0
+  self.cursor.y = 0
+  self.cursor.xDes = 0
+  self.cursor.yDes = 0
+  self.cursor.width = 0
+  self.cursor.height = 0
+  self.cursor.widthDes = 0
+  self.cursor.heightDes = 0
 end
 
 function MainMenu:update(dt)
@@ -32,19 +42,8 @@ function MainMenu:update(dt)
 
   if love.keyboard.wasPressed("s") or love.keyboard.wasPressed("down") then
     self.currentSelection = self.currentSelection + 1
-    if self.currentSelection > 1 then self.currentSelection = 0 end
+    if self.currentSelection > 1 then self.currentSelection = 1 end
   end
-end
-
-function MainMenu:render()
-  love.graphics.setFont(self.fontHeadingArimo)
-
-  local text = "Welcome to " .. Constants.TITLE .. " by " .. Constants.AUTHOR
-  love.graphics.printf(text, 0, love.graphics.getHeight() / 2 -200, love.graphics.getWidth(), "center")
-  
-  love.graphics.setFont(self.fontTextArimo)
-  love.graphics.printf(self.textStartGame, 0, love.graphics.getHeight() / 2 - 50, love.graphics.getWidth(), "center")
-  love.graphics.printf(self.textQuit, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
 
   local rectangleX = 0
   local rectangleY = 0
@@ -63,7 +62,33 @@ function MainMenu:render()
     rectangleWidth = textWidth
   end
 
-  love.graphics.rectangle("line", rectangleX, rectangleY, rectangleWidth, rectangleHeight)
+  rectangleWidth = rectangleWidth + 20
+  rectangleX = rectangleX - 10
+  rectangleHeight = rectangleHeight + 10
+  rectangleY = rectangleY - 5
+
+  self.cursor.xDes = rectangleX
+  self.cursor.yDes = rectangleY
+  self.cursor.widthDes = rectangleWidth
+  self.cursor.heightDes = rectangleHeight
+
+  self.cursor.x = self.cursor.x + ((self.cursor.xDes - self.cursor.x) * dt * 10)
+  self.cursor.y = self.cursor.y + ((self.cursor.yDes - self.cursor.y) * dt * 10)
+  self.cursor.width = self.cursor.width + ((self.cursor.widthDes - self.cursor.width) * dt * 10)
+  self.cursor.height = self.cursor.height + ((self.cursor.heightDes - self.cursor.height) * dt * 10)
+end
+
+function MainMenu:render()
+  love.graphics.setFont(self.fontHeadingArimo)
+
+  local text = "Welcome to " .. Constants.TITLE .. " by " .. Constants.AUTHOR
+  love.graphics.printf(text, 0, love.graphics.getHeight() / 2 -200, love.graphics.getWidth(), "center")
+  
+  love.graphics.setFont(self.fontTextArimo)
+  love.graphics.printf(self.textStartGame, 0, love.graphics.getHeight() / 2 - 50, love.graphics.getWidth(), "center")
+  love.graphics.printf(self.textQuit, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+
+  love.graphics.rectangle("line", self.cursor.x, self.cursor.y, self.cursor.width, self.cursor.height)
 end
 
 return MainMenu
