624aa35c97c244a9a07b5dc71d66c4e1c4b7e6fd
Jan-Henrik <jh.bruhn97@gmail.com>
2013-12-15 11:28:08 +0100

    rendering map as a background in main menu

--- src/menu/MainMenu.lua
+++ src/menu/MainMenu.lua
@@ -1,6 +1,7 @@
 local Constants = require 'conf'
 local class = require 'lib/middleclass'
 local Menu = require 'menu/Menu'
+local atl = require 'lib/advanced-tiled-loader/Loader'
 
 MainMenu = class('MainMenu', Menu)
 
@@ -24,8 +25,15 @@ function MainMenu:initialize()
   self.cursor.height = 0
   self.cursor.widthDes = 0
   self.cursor.heightDes = 0
+  
+  atl.path = Constants.ASSETS.MAPS
+
+  self.map = atl.load('Map.tmx')
+  self.map.drawObjects = false
 
   self.menuMusic = love.audio.newSource('assets/music/menu.mp3')
+
+  self.time = 0
 end
 
 function MainMenu:update(dt)
@@ -83,15 +91,40 @@ function MainMenu:update(dt)
   self.cursor.y = self.cursor.y + ((self.cursor.yDes - self.cursor.y) * dt * 10)
   self.cursor.width = self.cursor.width + ((self.cursor.widthDes - self.cursor.width) * dt * 10)
   self.cursor.height = self.cursor.height + ((self.cursor.heightDes - self.cursor.height) * dt * 10)
+
+  self.time = self.time + dt
 end
 
 function MainMenu:render()
-  love.graphics.setFont(self.fontHeadingArimo)
+  love.graphics.setBackgroundColor({255, 255, 255, 255})
 
-  local text = "Welcome to " .. Constants.TITLE .. " by " .. Constants.AUTHOR
-  love.graphics.printf(text, 0, love.graphics.getHeight() / 2 -200, love.graphics.getWidth(), "center")
+  love.graphics.push()
+
+  local translateX = 175 * math.cos(self.time) + 1200
+  local translateY = 175 * math.sin(self.time) + 800
+
+  love.graphics.translate(-translateX, -translateY)
+  self.map:autoDrawRange(-translateX, -translateY, 1, 0)
+
+  self.map:draw()
+  love.graphics.pop()
+
+
+  love.graphics.setFont(self.fontHeadingArimo)
   
+  local text = "Welcome to " .. Constants.TITLE .. " by " .. Constants.AUTHOR
+
+  love.graphics.setColor({255, 255, 255, 255})
+  love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - self.fontHeadingArimo:getWidth(text) / 2 - 20, love.graphics.getHeight() / 2 - 210, self.fontHeadingArimo:getWidth(text) + 40, self.fontHeadingArimo:getHeight() + 20)
+  
+  love.graphics.setColor({0, 0, 0, 255})
+  love.graphics.printf(text, 0, love.graphics.getHeight() / 2 -200, love.graphics.getWidth(), "center")
+
+  love.graphics.setColor({255, 255, 255, 255})
+  love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - math.max(self.fontHeadingArimo:getWidth(self.textStartGame), self.fontTextArimo:getWidth(self.textQuit)) / 2, love.graphics.getHeight() / 2 - 70,  math.max(self.fontHeadingArimo:getWidth(self.textStartGame), self.fontTextArimo:getWidth(self.textQuit)), self.fontTextArimo:getHeight() * 2 + 50)
+ 
   love.graphics.setFont(self.fontTextArimo)
+  love.graphics.setColor({0, 0, 0, 255})
   love.graphics.printf(self.textStartGame, 0, love.graphics.getHeight() / 2 - 50, love.graphics.getWidth(), "center")
   love.graphics.printf(self.textQuit, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
 
