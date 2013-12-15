6fa49e3956f171a90d5eb99e6787b1076421a1a0
Sören Gade <gade.soeren@googlemail.com>
2013-12-15 19:15:36 +0100

    Extended main menu.

--- src/menu/MainMenu.lua
+++ src/menu/MainMenu.lua
@@ -56,7 +56,7 @@ function MainMenu:update(dt)
 
   if love.keyboard.wasPressed("s") or love.keyboard.wasPressed("down") then
     self.currentSelection = self.currentSelection + 1
-    if self.currentSelection > 1 then self.currentSelection = 1 end
+    if self.currentSelection > 3 then self.currentSelection = 3 end
   end
 
   local rectangleX = 0
@@ -70,9 +70,18 @@ function MainMenu:update(dt)
     rectangleY = love.graphics.getHeight() / 2 - 50
     rectangleWidth = textWidth
   elseif self.currentSelection == 1 then
-    local textWidth = self.fontTextArimo:getWidth(self.textQuit)
+    local textWidth = self.fontTextArimo:getWidth(self.textStartServer)
     rectangleX = love.graphics.getWidth() / 2 - textWidth / 2
     rectangleY = love.graphics.getHeight() / 2
+  elseif self.currentSelection == 2 then
+    local textWidth = self.fontTextArimo:getWidth(self.textStartClient)
+    rectangleX = love.graphics.getWidth() / 2 - textWidth / 2
+    rectangleY = love.graphics.getHeight() / 2 + 50
+    rectangleWidth = textWidth
+  elseif self.currentSelection == 3 then
+    local textWidth = self.fontTextArimo:getWidth(self.textQuit)
+    rectangleX = love.graphics.getWidth() / 2 - textWidth / 2
+    rectangleY = love.graphics.getHeight() / 2 + 100
     rectangleWidth = textWidth
   end
 
@@ -120,12 +129,14 @@ function MainMenu:render()
   love.graphics.printf(text, 0, love.graphics.getHeight() / 2 -200, love.graphics.getWidth(), "center")
 
   love.graphics.setColor({255, 255, 255, 255})
-  love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - math.max(self.fontHeadingArimo:getWidth(self.textStartGame), self.fontTextArimo:getWidth(self.textQuit)) / 2, love.graphics.getHeight() / 2 - 70,  math.max(self.fontHeadingArimo:getWidth(self.textStartGame), self.fontTextArimo:getWidth(self.textQuit)), self.fontTextArimo:getHeight() * 2 + 50)
+  love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - self.fontTextArimo:getWidth(self.textStartClient) / 2, love.graphics.getHeight() / 2 - 70,  math.max(self.fontHeadingArimo:getWidth(self.textStartGame), self.fontTextArimo:getWidth(self.textStartClient)), self.fontTextArimo:getHeight() * 2 + 150)
  
   love.graphics.setFont(self.fontTextArimo)
   love.graphics.setColor({0, 0, 0, 255})
   love.graphics.printf(self.textStartGame, 0, love.graphics.getHeight() / 2 - 50, love.graphics.getWidth(), "center")
-  love.graphics.printf(self.textQuit, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+  love.graphics.printf(self.textStartServer, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+  love.graphics.printf(self.textStartClient, 0, love.graphics.getHeight() / 2 + 50, love.graphics.getWidth(), "center")
+  love.graphics.printf(self.textQuit, 0, love.graphics.getHeight() / 2 + 100, love.graphics.getWidth(), "center")
 
   love.graphics.rectangle("line", self.cursor.x, self.cursor.y, self.cursor.width, self.cursor.height)
 end
