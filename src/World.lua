9932dbcf2dc134917a400aacc164ae7094a23624
Sören Gade <gade.soeren@googlemail.com>
2013-12-16 19:24:00 +0100

    Added whitespace.

--- src/World.lua
+++ src/World.lua
@@ -1,9 +1,9 @@
-function string.starts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
+function string.starts(String, Start)
+   return string.sub(String, 1, string.len(Start)) == Start
 end
 
-function string.ends(String,End)
-   return End=='' or string.sub(String,-string.len(End))==End
+function string.ends(String, End)
+   return End == '' or string.sub(String, -string.len(End)) == End
 end
 
 local Constants = require 'conf'
