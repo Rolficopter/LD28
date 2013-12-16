4add473535dcb3d6cb61f58cc93d56f5300672a6
Sören Gade <gade.soeren@googlemail.com>
2013-12-16 17:58:23 +0100

    Implemented NetworkInput.

--- src/input/NetworkInput.lua
+++ src/input/NetworkInput.lua
@@ -1,27 +1,72 @@
-local InputSource = require 'input/InputSource'
+local KeyboardAndMouseInput = require 'input/KeyboardAndMouseInput'
 
-NetworkInput = class('NetworkInput', InputSource)
+NetworkInput = class('NetworkInput', KeyboardAndMouseInput)
 
-function NetworkInput:initialize(world)
+function NetworkInput:initialize(world, networkClient, isRemote)
 	InputSource:initialize(world)
+	self.isRemote = isRemote -- is the client 
+	self.networkClient = networkClient
+
+	self.lastDirection = InputSource.Direction.none
+	self.lastArmAngle = 0
+	self.lastShouldJump = false
+end
+
+function NetworkInput:sendMessage(message, data)
+	local msg = message
+	if data then
+		msg = msg .. ':' .. data
+	end
+
+	print("Sending " .. message .. ", data:", data)
+	self.networkClient:send(msg)
 end
 
 function NetworkInput:updateFromExternalInput(networkClientData)
-	InputSource:updateFromExternalInput(networkClientData)
+	KeyboardAndMouseInput:updateFromExternalInput(networkClientData)
 
 	error("Not implemented")
 end
 
 function NetworkInput:shouldJump()
-	return false
+	self.lastShouldJump = false
+
+	if not self.isRemote then
+		local shouldJump = KeyboardAndMouseInput:shouldJump()
+
+		if shouldJump then
+			self.lastShouldJump = shouldJump
+			self:sendMessage('jump')
+		end
+	end
+
+	return self.lastShouldJump
 end
 
 function NetworkInput:getDirection()
-	return InputSource.Direction.none
+	if not self.isRemote then
+		local direction = KeyboardAndMouseInput:getDirection()
+
+		if ( direction ~= self.lastDirection ) then
+			self.lastDirection = direction
+			self:sendMessage('direction', direction)
+		end
+	end
+
+	return self.lastDirection
 end
 
 function NetworkInput:getArmAngle()
-	return 0
+	if not self.isRemote then
+		local armAngle = KeyboardAndMouseInput:getArmAngle(0, 0)
+
+		if ( armAngle ~= self.lastArmAngle ) then
+			self.lastArmAngle = armAngle
+			self:sendMessage('armAngle', armAngle)
+		end
+	end
+
+	return self.lastArmAngle
 end
 
 return NetworkInput
