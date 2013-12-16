b90d6ec2f36db2c042c8f3d0e995ccca91d6d5c5
Sören Gade <gade.soeren@googlemail.com>
2013-12-16 18:29:11 +0100

    Made server work based on callbacks.

--- src/Server.lua
+++ src/Server.lua
@@ -4,21 +4,44 @@ require 'lib/LUBE'
 
 Server = class('Server', Updateable)
 
+local instance = nil
+local server_connect = function(clientID)
+	instance:_connect(clientID)
+end
+local server_disconnect = function(clientID)
+	instance:onClientLost(clientID)
+end
+local server_recv = function(data, clientID)
+	instance:onData(data, clientID)
+end
+
+
 local initServer = function(self)
 	self.server = lube.udpServer()
 	self.server.handshake = Constants.NET.HANDSHAKE
 	self.server:setPing(true, Constants.NET.PING.TIMEOUT * 3, Constants.NET.PING.MSG)
+
+	self.server.callbacks.recv = server_recv
+	self.server.callbacks.connect = server_connect
+	self.server.callbacks.disconnect = server_disconnect
+
 	self.server:listen(Constants.NET.PORT)
 	print("Server listening on port " .. Constants.NET.PORT .. ".")
 end
 function Server:initialize()
 	Updateable:initialize()
+	instance = self
 
 	initServer(self)
 	self.clientIds = {}
 end
 
-function Server:sendData(data, senderClientID)
+function Server:update( dt )
+	self.server:update(dt)
+end
+
+function Server:sendBroadcast(data, senderClientID)
+	-- send message to all except for one
 	if data then
 		for key, value in pairs(self.clientIds) do
 			if ( key ~= senderClientID ) then
@@ -28,39 +51,45 @@ function Server:sendData(data, senderClientID)
 	end
 end
 function Server:sendMessage(data, receiverClientID)
+	-- send message to one client
+	print("Message to client.", "ID:", receiverClientID, "Data:", data)
 	self.server:send(data, receiverClientID)
 end
 
-function Server:onNewClient( clientID )
+-- callbacks
+-- connect
+local saveClientID = function(self, clientID)
+	if clientID and not self.clientIds[clientID] then
+		self.clientIds[clientID] = true
+
+		return true
+	end
+	return false
+end
+function Server:_connect(clientID)
+	if saveClientID(self, clientID) then
+		self:onNewClient(clientID)
+	end
+end
+function Server:onNewClient(clientID)
 	print("New client:", clientID)
 
 	self:sendMessage('Map:Map', clientID)
 end
+-- disconnect
+function Server:onClientLost(clientID)
+	print("Client disconnected:", clientID)
 
-local saveClientID = function(self, clientID)
-	if clientID and not self.clientIds[clientID] then
-		self.clientIds[clientID] = true
-		-- new client
-		self:onNewClient(clientID)
-	end
+	self:sendMessage()
 end
-local processClientMessages = function(self)
-	local data, clientID = self.server:receive()
-	if ( data and clientID ) then
-		saveClientID(self, clientID)
-
-		if data == Constants.NET.PING.MSG then
-			-- don't transfer ping messages
-			return
-		end
-
-		self:sendData(data, clientID)
+-- recv
+function Server:onData(data, clientID)
+	if data == Constants.NET.PING.MSG then
+		-- don't transfer ping messages
+		return
 	end
-end
-function Server:update( dt )
-	self.server:update(dt)
 
-	processClientMessages(self)
+	self:sendBroadcast(data, clientID)
 end
 
 return Server
